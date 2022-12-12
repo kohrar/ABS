ce0b184ee36e1dfb57931ef3db7523229b63b2d5
Leo Leung <leo@steamr.com>
2022-12-12 15:08:14 -0700

    Updated user agent

--- src/constants.js
+++ src/constants.js
@@ -48,7 +48,7 @@ const constants = Object.freeze({
   NUM_DAILY_TREND_FETCHES: 4,
   // TODO: add more mobile user agents
   MOBILE_USER_AGENTS: Object.freeze([
-    'Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Mobile Safari/537.36 Edg/86.0.622.51',
+    'Mozilla/5.0 (Linux; Android 12; Pixel 3a) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36 EdgA/107.0.1418.28',
   ]),
-  EDGE_USER_AGENT: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.51',
+  EDGE_USER_AGENT: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.37',
 });
